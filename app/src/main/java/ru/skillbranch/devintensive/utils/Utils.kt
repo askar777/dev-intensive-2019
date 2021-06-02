08c818a1e77699ca2ab0b14184d9a916f9c98cb4
Askar <askartursyn7@gmail.com>
2021-06-03 00:53:09 +0600

    Commit all tasks Done

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -5,7 +5,6 @@ import java.util.*
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?>{
         val parts: List<String>? = fullName?.split(" ")
-
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
         return firstName to lastName
